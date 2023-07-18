42fbbe535da5820bbad670637ba25ef4da859a0c
joaquin mansilla <joaquinmansilla@fiqus.coop>
2023-07-18 20:28:26 -0300

    remove menu when enter in game

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -2,7 +2,7 @@ mod components;
 mod styles;
 mod systems;
 
-use crate::main_menu::systems::layout::spawn_main_menu;
+use crate::main_menu::systems::layout::{despawn_main_menu, spawn_main_menu};
 use crate::AppState;
 use bevy::prelude::*;
 
@@ -10,6 +10,7 @@ pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)));
+        app.add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
 }
--- src/main_menu/systems/layout.rs
+++ src/main_menu/systems/layout.rs
@@ -5,6 +5,12 @@ pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     _ = build_main_menu(&mut commands, &asset_server);
 }
 
+pub fn despawn_main_menu(mut commands: Commands, main_menu: Query<Entity, With<MainMenu>>) {
+    if let Ok(main_menu) = main_menu.get_single() {
+        commands.entity(main_menu).despawn()
+    }
+}
+
 fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
     let main_menu = commands
         .spawn((
